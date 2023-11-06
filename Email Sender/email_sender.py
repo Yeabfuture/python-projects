211a91f7f67c5ab414a1f9de186fa9620a557a71
Yeabsira <yeabsiramersha58@yeabfuture.com>
2023-11-06 17:29:46 +0300

    email sender project

---
+++ Email Sender/email_sender.py
@@ -0,0 +1,33 @@
+from email.message import EmailMessage
+import ssl
+import smtplib
+import json
+
+# Load configuration from config.json
+with open('C:/python projects/Email Sender/config.json', 'r') as file:
+    config = json.load(file)
+
+email_sender = 'yeabsiramersha58@gmail.com'
+email_password = config['email_password']  # Access the password from the config
+
+email_receiver = 'yeabsiramersha58@yeabfuture.com'
+
+subject = "Write your email subject here"
+
+body = '''
+This is a sample email body.
+'''
+
+em = EmailMessage()
+em['From'] = email_sender
+em['To'] = email_receiver
+em['Subject'] = subject
+em.set_content(body)
+
+context = ssl.create_default_context()
+
+with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+    smtp.login(email_sender, email_password)
+    smtp.sendmail(email_sender, email_receiver, em.as_string())
+
+print("Email sent successfully!")
---
+++ Email Sender/pass.py
@@ -0,0 +1,1 @@
+password = "rvmd yaek oyqx swuh"
